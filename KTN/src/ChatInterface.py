7cddb10917c23a963c03e51af21f986961f14761
Øyvind Robertsen <oyvind@arcticstyle.net>
2014-03-05 13:20:45 +0100

    interface fixes

--- KTN/src/ChatInterface.py
+++ KTN/src/ChatInterface.py
@@ -49,8 +49,12 @@ class ChatInterface(object):
 
     def display_messages_and_prompt(self):
         if self.client.messages:
-            for i in xrange(len(self.client.messages)):
-                self.stdscr.addstr(i, 0, self.client.messages[i])
+            if len(self.client.messages) < self.NR_OF_MESSAGES_TO_DISPLAY:
+                for i in xrange(len(self.client.messages)):
+                    self.stdscr.addstr(i, 0, self.client.messages[i])
+            else:
+                for i in xrange(len(self.NR_OF_MESSAGES_TO_DISPLAY)):
+                    self.stdscr.addstr(i, 0, self.client.messages[len(self.client.messages) - i])
         self.stdscr.addstr(self.NR_OF_MESSAGES_TO_DISPLAY, 0, '--> Enter a message:')
         self.stdscr.move(self.NR_OF_MESSAGES_TO_DISPLAY + 1, 0)
         self.stdscr.refresh()
@@ -61,7 +65,7 @@ class ChatInterface(object):
         if message == 'logout':
             self.client.logout()
         else:
-            self.client.send_message('test')
+            self.client.send_message(message)
 
 def get_args():
     parser = argparse.ArgumentParser(description='JSON based chat.',
