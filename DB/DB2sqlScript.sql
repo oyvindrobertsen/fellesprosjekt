1ff269fb1bc42f1f0b22017b6de3eccff82405ad
tbstbs <tbstbs4@gmail.com>
2014-03-10 11:49:53 +0100

    fixed sql script
    added foreign key constraints and fixed minor faults

--- DB/DB2sqlScript.sql
+++ DB/DB2sqlScript.sql
@@ -1,47 +1,21 @@
+USE apotychia;
+
 CREATE TABLE person (
-	username VARCHAR(45) NOT NULL,
-	password VARCHAR(45) NOT NULL,
-	name	 VARCHAR(45) NOT NULL, 
-	mail 	 VARCHAR(45) NOT NULL, 
+	username     VARCHAR(45) NOT NULL,
+	pwd 	 	 VARCHAR(45) NOT NULL,
+	firstName	 VARCHAR(45) NOT NULL,
+	lastName	 VARCHAR(45) NOT NULL,
+	mail 	 VARCHAR(45) 	 NOT NULL, 
 
 	PRIMARY KEY (username)
-)
+);
 
+CREATE TABLE room (
+	roomNr 		VARCHAR(10)		NOT NULL,
+	capacity 	INT 			NOT NULL, 
 
-CREATE TABLE event (
-	eventID 		INT  		 NOT NULL AUTO_INCREMENT,
-	name 			VARCHAR(45)	 NOT NULL,
-	start   		DATETIME	 NOT NULL, 
-	end 			DATETIME     NOT NULL,
-	isActive 		VARCHAR(45)  NOT NULL,
-	description		TINYTEXT	 NULL,
-	eventAdmin		VARCHAR(45)  NOT NULL,
-	participantID   INT 		 NULL, 
-
-	PRIMARY KEY (eventID),
-	FOREIGN KEY(particpant) REFERENCES particpants(id)
-)
-
-
-CREATE TABLE participants (
-	id 		 	INT		 		NOT NULL	AUTO_INCREMENT,
-	username 	VARCHAR(45) 	NULL,
-	groupID	 	INT 			NULL, 
-
-	PRIMARY KEY (id),
-	FOREIGN KEY(username) REFERENCES person (username),
-	FOREIGN KEY(groupID)  REFERENCES group (groupID)
-)
-
-
-CREATE TABLE group (
-	groupID  		INT		NOT NULL 	AUTO_INCREMENT,
-	memberID  		INT 	NULL,
-
-	PRIMARY KEY (groupID),
-	FOREIGN KEY (memberID) REFERENCES memberOf (id)
-)
-
+	PRIMARY KEY (roomNr)
+);
 
 CREATE TABLE memberOf (
 	id 			INT 			NOT NULL	AUTO_INCREMENT,
@@ -49,7 +23,46 @@ CREATE TABLE memberOf (
 
 	PRIMARY KEY (id),
 	FOREIGN KEY (username)  REFERENCES person(username)
-)
+		ON UPDATE cascade ON DELETE cascade
+);
+
+CREATE TABLE eventGroup (
+	groupID  		INT		NOT NULL 	AUTO_INCREMENT,
+	memberID  		INT 	NULL,
+
+	PRIMARY KEY (groupID),
+	FOREIGN KEY (memberID) REFERENCES memberOf (id)
+	ON UPDATE cascade ON DELETE SET NULL
+);
+
+CREATE TABLE participants (
+	id		 	INT				NOT NULL	AUTO_INCREMENT,
+	username 	VARCHAR(45) 	NULL,
+	groupID	 	INT				NULL, 
+	
+	PRIMARY KEY (id),
+	FOREIGN KEY(username) REFERENCES person (username)
+		ON UPDATE cascade ON DELETE SET NULL,
+	FOREIGN KEY(groupID)  REFERENCES eventGroup (groupID)
+		ON UPDATE cascade ON DELETE SET NULL
+);
+
+CREATE TABLE calendarEvent (
+	eventID 		INT  		 NOT NULL AUTO_INCREMENT,
+	eventName 		VARCHAR(45)	 NOT NULL,
+	startTime  		DATETIME	 NOT NULL, 
+	endTime			DATETIME     NOT NULL,
+	isActive 		VARCHAR(45)  NOT NULL,
+	description		TINYTEXT	 NULL,
+	eventAdmin		VARCHAR(45)  NOT NULL,
+	participantID   INT			 NULL, 
+
+	PRIMARY KEY (eventID),
+	FOREIGN KEY (participantID) REFERENCES participants (id)
+		ON UPDATE cascade ON DELETE SET NULL
+);
+
+
 
 CREATE TABLE booked (
 	bookedID 	INT 			NOT NULL	AUTO_INCREMENT,
@@ -57,17 +70,9 @@ CREATE TABLE booked (
 	roomNr		VARCHAR(10)		NOT NULL,
 
 	PRIMARY KEY (bookedID),
-	FOREIGN KEY (eventID) REFERENCES (event),
-	FOREIGN KEY (roomNr)  REFERENCES (room)	
-)
-
-
-CREATE TABLE room (
-	roomNr 		VARCHAR(10)		NOT NULL,
-	capacity 	INT 			NOT NULL, 
-
-	PRIMARY KEY (roomNr)
-)
-
-
+	FOREIGN KEY (eventID) REFERENCES  calendarEvent(eventID)
+		ON UPDATE cascade ON DELETE cascade,
+	FOREIGN KEY (roomNr)  REFERENCES room(roomNr)
+		ON UPDATE cascade ON DELETE cascade
+);
 
