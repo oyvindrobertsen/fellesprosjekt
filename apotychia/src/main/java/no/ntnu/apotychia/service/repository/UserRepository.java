2f3e8982971aafaef9ba3551308a27e7d7a4c1ee
Øyvind Robertsen <oyvind@arcticstyle.net>
2014-03-12 17:23:55 +0100

    UserRepository

--- apotychia/src/main/java/no/ntnu/apotychia/service/repository/UserRepository.java
+++ apotychia/src/main/java/no/ntnu/apotychia/service/repository/UserRepository.java
@@ -20,6 +20,7 @@ public class UserRepository {
     private JdbcTemplate jt;
 
     public User findOne(String username) {
+        logger.info("Looking up user:" + username);
         User result = jt.queryForObject("SELECT * FROM PERSON WHERE username = ?",
                 new Object[] {username},
                 new RowMapper<User>() {
@@ -38,7 +39,7 @@ public class UserRepository {
     }
 
     public void insert(User user) {
-        logger.info("Inserting user with hashed password:" + user.getEncodedPassword() + ", with size:" + user.getEncodedPassword().length());
+        logger.info("Inserting user:" + user.getUsername());
         jt.update(
                 "INSERT INTO person VALUES (?, ?, ?, ?, ?)",
                 new Object[] {user.getUsername(), user.getEncodedPassword(), user.getFirstName(),
