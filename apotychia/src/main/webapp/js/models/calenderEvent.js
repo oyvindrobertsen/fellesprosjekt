2d9c45b5b592eaa3e987cd5d030d77d989f3f750
Andreas Lepperød <andreas_lepp@hotmail.com>
2014-03-17 11:12:53 +0100

    more js

---
+++ apotychia/src/main/webapp/js/models/calenderEvent.js
@@ -0,0 +1,8 @@
+App.CalenderEvent = DS.Model.extend({
+    eventName: DS.attr('string')
+    //startTime: DS.attr('date')
+    //endTime: DS.attr('date')
+    //isActive: DS.attr('boolean')
+    //description: DS.attr('string')
+    //eventAdmin: DS.attr('person')
+});
---
+++ apotychia/src/main/webapp/js/models/person.js
@@ -0,0 +1,3 @@
+App.Person = DS.Model.extend({
+    name: DS.attr('string')
+});
